a368b987237aa6a4237c8b0436c817ab9bcc5404
Guo Yu <i@guoyu.me>
2014-11-09 20:33:10 +0800

    Add templates and js to seed project

---
+++ www/js/app.js
@@ -0,0 +1,54 @@
+;(function(angular, debug) {
+
+  angular
+    .module('project', [
+      'ionic', 
+      'avoscloud',
+      'avoscloud-auth'
+    ])
+    .run(['$ionicPlatform', init])
+    .config(['$stateProvider', '$urlRouterProvider', 'avoscloudProvider', config]);
+
+  function init($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      // org.apache.cordova.statusbar required
+      if (window.StatusBar)
+        StatusBar.styleDefault();
+    });
+  }
+
+  function config($stateProvider, $urlRouterProvider, avoscloudProvider) {
+    // Appkey and AppId configs
+    avoscloudProvider.config({
+      appId: 'xxx',
+      appKey: 'xxx'
+    });
+
+    // States Routers
+    $stateProvider
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'templates/signup.html',
+        controller: 'sign'
+      })
+      .state('signin', {
+        url: '/signin',
+        templateUrl: 'templates/signin.html',
+        controller: 'sign'
+      })
+      // setup an abstract state for the tabs directive
+      // .state('tab', {
+      //   url: "/tab",
+      //   abstract: true,
+      //   templateUrl: "templates/tabs.html"
+      // })
+    
+    // 404 Router
+    $urlRouterProvider.otherwise('/');
+  }
+
+})(window.angular, window.debug);
